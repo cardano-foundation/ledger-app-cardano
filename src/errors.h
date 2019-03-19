92f25b4ca88e2bd5806a4f88af6b81841e80af84
Peter Peresini <peter.peresini@gmail.com>
2019-03-19 11:48:53 +0100

    Remove obsolete error codes

--- src/errors.h
+++ src/errors.h
@@ -1,15 +1,6 @@
 #ifndef H_CARDANO_APP_ERRORS
 #define H_CARDANO_APP_ERRORS
 
-// exception codes
-#define SW_DEVELOPER_ERR 0x6B00
-#define SW_INVALID_PARAM 0x6B01
-#define SW_IMPROPER_INIT 0x6B02
-
-// Still in some call
-
-#define SW_USER_REJECTED 0x6985
-
 enum {
 	// Successfull responses
 	SUCCESS                 = 0x9000,
